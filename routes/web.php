be792bec897faa7a2b980bd4b32788cb1c21060a
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-02-27 02:14:22 +0000

    add router for login and dash

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('client.dashClient');
 });
+Route::get('/',[HomeController::class , 'index'])->name('dash');
 
 //register
-Route::get('/register',[AuthController::class , 'register']);
-Route::post('/user/store',[AuthController::class , 'store'])->name('user.store');
+Route::get('/register',[AuthController::class , 'register'])->name('register');
+Route::post('/register',[AuthController::class , 'store']);
 //login
 Route::get('/login',[AuthController::class , 'login'])->name('login');
 Route::post('/login',[AuthController::class , 'authenticate']);
+
