c483808d7947f35d27c196017e8c37f7edbf4641
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-03-01 01:22:00 +0000

    ajoute index for display ticket paginat

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
 public function index(){
-    return view('Dashboard');
+
+    $tickets = Ticket::where('user_id', Auth::id())->latest()->paginate(3);
+
+
+        if (request()->has('search')) {
+            $search = request()->get('search', '');
+
+            $tickets = $tickets->where(function ($query) use ($search) {
+                $query->where('title', 'like', '%' . $search . '%')
+                      ->orWhere('location', 'like', '%' . $search . '%')
+                      ->orWhere('content', 'like', '%' . $search . '%');
+
+            });
+        }
+    return view('Dashboard',compact('tickets'));
 
 }
 
