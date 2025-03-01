df5fe2958fa282625f57088f8e0995b9f3e3453e
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-03-01 01:24:11 +0000

    routage for request and reponse

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
-
-use App\Http\Controllers\HomeController;
+namespace App\Http\Controllers;
+// use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+// use App\Http\Controllers\AuthController;
+// use App\Http\Controllers\TicketController;
 
 Route::get('/', function () {
     return view('client.dashClient');
-});
-Route::get('/',[HomeController::class , 'index'])->name('dash');
+})->middleware('auth');
+Route::get('/',[HomeController::class , 'index'])->name('dash')->middleware('auth');
 
 //register
 Route::get('/register',[AuthController::class , 'register'])->name('register');
@@ -16,3 +17,14 @@ Route::post('/register',[AuthController::class , 'store']);
 Route::get('/login',[AuthController::class , 'login'])->name('login');
 Route::post('/login',[AuthController::class , 'authenticate']);
 
+//grouping routages Tikcet
+Route::group(['prefix' => 'Ticket/','as' => 'Ticket.','middleware' => 'auth'], function(){
+    Route::get('/', [TicketController::class, 'index'])->name('index');
+    Route::get('/create', [TicketController::class, 'create'])->name('form');
+    Route::post('/create/{user}', [TicketController::class, 'store'])->name('create');
+    Route::get('/{id}', [TicketController::class, 'show'])->name('show');
+    Route::get('/{id}/edit', [TicketController::class, 'edit'])->name('edit');
+    Route::put('/{id}', [TicketController::class, 'update'])->name('update');
+    Route::delete('/{id}', [TicketController::class, 'destroy'])->name('delete');
+    // Route::post('/{id}/reponses', [ReponseController::class, 'store'])->name('reponsestore');
+});
